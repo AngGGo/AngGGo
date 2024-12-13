3521d419020a82f503a239542b9754dfe92827da
sh97b <sh097.b@gmail.com>
2024-12-13 18:41:35 +0900

    sh_1213

--- develope/반서현/ag01_test/ag01/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include("home.urls")), # 메인페이지
-    path('member/', include("member.urls")), # 로그인페이지
-    path('event/', include("event.urls")), # 로그인페이지
-]
